16ff931ec3313f6ed27115b898280f5bf3656387
Kevin Wang <kev.wang424@gmail.com>
2016-11-16 01:02:42 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,16 @@
 
 def key_for_min_value(name_hash)
 
+  return nil if name_hash.empty?
+
+  min_value = 1.0/0.0         #this represents infinity. we dont know min value in the hash so set as infinity
+  min_key = ""                #defaults as string since you cannot default as symbol
+  name_hash.each do |key,value|
+    if value < min_value
+      min_value = value
+      min_key = key.to_s     #changes to string because i cannot just simply reassign
+    end
+  end
+  min_key.to_sym            #convert back to symbol so i get the right answer
+
 end
